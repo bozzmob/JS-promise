de9c6c116517635fa660861fdc7769938b20b106
satvik <satvik.jagannath7@gmail.com>
2015-12-05 00:27:41 +0530

    Debug Mode #1

--- JS-Promise/script.js
+++ JS-Promise/script.js
@@ -1,14 +1,19 @@
-'use strict';
-
-function testPromise () {
+function testPromise() {
   log = document.getElementById('log');
   log.innerHTML = "<code>Promise<code> is supported! YAY!";
+  console.log("testPromise");
 }
+
+function execstart(){
+console.log("execstart");
 if ("Promise" in window) {
   var testbtn = document.getElementById("btn");
    testbtn.addEventListener("click",testPromise);
+   console.log("if");
 }
 else {
   log = document.getElementById('log');
   log.innerHTML = "Live example not available as your browser doesn't support the <code>Promise<code> interface.";
+  console.log("else");
+}
 }
